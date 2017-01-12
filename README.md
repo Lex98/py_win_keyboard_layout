1c17392bb1ed8250ce6a46be30ac741b79e94407
Aleksey Korenskiy <Lexis098@gmail.com>
2017-01-12 20:58:28 +0300

    base function

--- README.md
+++ README.md
@@ -2,7 +2,11 @@ py_win_keyboard_layout
 ========
 
 Interaction with keyboard layout on windows
+Tested only on windows 7 x64 with pywin32 build 220
 
+## Dependency
+
+[pywin32](https://sourceforge.net/projects/pywin32/?source=directory)
 
 ## Usage
 
@@ -38,17 +42,62 @@ py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04190419)
 
 <a name="py_win_keyboard_layout.get_foreground_window_keyboard_layout"/>
 ## py_win_keyboard_layout.**get\_foreground\_window\_keyboard\_layout**
+    Returns foreground window keyboard layout as integer
+
+    Examples:
+    68748313 - 0x04190419 - russian
+    67699721 - 0x04090409 - english
 
 
 <a name="py_win_keyboard_layout.change_foreground_window_keyboard_layout"/>
 ## py_win_keyboard_layout.**change\_foreground\_window\_keyboard\_layout**
+Change foreground window keyboard layout
 
+    Parameter
+
+        layout_id=0 : integer
+
+        Integer containing a locale id, eg 68748313 - 0x04190419 - russian
+
+        Default change layout like Ctrl+Shift or Alt+Shift
+
+    Return Value
+
+    Returns True if layout is changed
 
 <a name="py_win_keyboard_layout.get_keyboard_layout_list"/>
 ## py_win_keyboard_layout.**get\_keyboard\_layout\_list**
+Returns a tuple of all locale ids currently loaded
 
+    Example
+
+    (68748313, 67699721)
 
 
 
 <a name="py_win_keyboard_layout.load_keyboard_layout"/>
 ### py_win_keyboard_layout.**load\_keyboard\_layout**
+
+Loads a new locale id
+
+    Parameters
+
+        string_layout_id : string
+
+        Hex string containing a locale id, eg "00000409"
+
+        Flags=0 : int
+
+        Combination of win32con.KLF_* constants
+
+        Examples
+
+        KLF_ACTIVATE|KLF_SETFORPROCESS|KLF_REORDER == 0x109 == 265
+
+    Return Value
+
+    Returns the integer locale id that was loaded
+
+    Example
+
+    load_keyboard_layout("00000409") == 67699721 for english
--- py_win_keyboard_layout/__init__.py
+++ py_win_keyboard_layout/__init__.py
@@ -1,5 +1,5 @@
 """
-
+Interaction with keyboard layout on windows
 """
 
 
@@ -11,6 +11,13 @@ WM_INPUTLANGCHANGEREQUEST = 0x0050
 
 
 def get_foreground_window_keyboard_layout():
+    """
+    Returns foreground window keyboard layout as integer
+
+    Examples:
+    68748313 - 0x04190419 - russian
+    67699721 - 0x04090409 - english
+    """
     window_handle = win32gui.GetForegroundWindow()
     thread_id = win32process.GetWindowThreadProcessId(window_handle)[0]
     layout_id = win32api.GetKeyboardLayout(thread_id)
@@ -18,6 +25,21 @@ def get_foreground_window_keyboard_layout():
 
 
 def change_foreground_window_keyboard_layout(layout_id=0):
+    """
+    Change foreground window keyboard layout
+
+    Parameter
+
+        layout_id=0 : integer
+
+        Integer containing a locale id, eg 68748313 - 0x04190419 - russian
+
+        Default change layout like Ctrl+Shift or Alt+Shift
+
+    Return Value
+
+    Returns True if layout is changed
+    """
     window_handle = win32gui.GetForegroundWindow()
     return not win32api.SendMessage(window_handle,
                                     WM_INPUTLANGCHANGEREQUEST,
@@ -26,8 +48,40 @@ def change_foreground_window_keyboard_layout(layout_id=0):
 
 
 def get_keyboard_layout_list():
+    """
+    Returns a tuple of all locale ids currently loaded
+
+    Example
+
+    (68748313, 67699721)
+    """
     return win32api.GetKeyboardLayoutList()
 
 
-def load_keyboard_layout(layout_id, flags=0):
-    return win32api.LoadKeyboardLayout(layout_id, flags)
+def load_keyboard_layout(string_layout_id, flags=0):
+    """
+    Loads a new locale id
+
+    Parameters
+
+        string_layout_id : string
+
+        Hex string containing a locale id, eg "00000409"
+
+        Flags=0 : int
+
+        Combination of win32con.KLF_* constants
+
+        Examples
+
+        KLF_ACTIVATE|KLF_SETFORPROCESS|KLF_REORDER == 0x109 == 265
+
+    Return Value
+
+    Returns the integer locale id that was loaded
+
+    Example
+
+    load_keyboard_layout("00000409") == 67699721 for english
+    """
+    return win32api.LoadKeyboardLayout(string_layout_id, flags)
